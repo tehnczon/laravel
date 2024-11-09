610a914ad38b17f7d187b79c8805fd1cf22f12b6
tehnczon <147896311+tehnczon@users.noreply.github.com>
2024-11-09 12:29:16 +0800

    Update ProductSeeder.php
    prodseeder update

--- my-app/database/seeders/ProductSeeder.php
+++ my-app/database/seeders/ProductSeeder.php
@@ -2,16 +2,34 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Product;
+use App\Models\Category;
 
 class ProductSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        //
+        $products = [
+            [
+                'name' => 'Product 1',
+                'description' => 'Description for product 1',
+                'price' => 100.00,
+            ],
+            [
+                'name' => 'Product 2',
+                'description' => 'Description for product 2',
+                'price' => 200.00,
+            ],
+            [
+                'name' => 'Product 3',
+                'description' => 'Description for product 3',
+                'price' => 300.00,
+            ],
+        ];
+
+        foreach ($products as $product) {
+            $productModel = Product::create($product);
+        }
     }
 }
