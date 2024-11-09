2b3c5ea064715c735d59684e8f9ec372c501de71
tehnczon <147896311+tehnczon@users.noreply.github.com>
2024-11-09 12:16:16 +0800

    Update 2024_11_07_162046_create_products_table.php

--- my-app/database/migrations/2024_11_07_162046_create_products_table.php
+++ my-app/database/migrations/2024_11_07_162046_create_products_table.php
@@ -13,9 +13,9 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table-string ('name');
-            $table-string ('name');
-            $table-›text ('description');
+            $table->string ('name');
+            $table->string ('name');
+            $table->text ('description');
             $table->timestamps();
         });
     }
